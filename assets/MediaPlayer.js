d3c2049f856e678c0caedc124eb3900356f50aef
Nicolas Bustos <jnbustosr@unal.edu.co>
2022-02-14 09:41:49 -0500

    MediaPlayer plugin player encapsulated

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -6,8 +6,34 @@ function MediaPlayer(config) {
   }
 
   MediaPlayer.prototype._initPlugins = function () {
+    const player = {
+      play: () => this.play(),
+      pause: () => this.pause(),
+      // Inside the get & set functions 'this' changes from MediaPlayer scope to the player object scope
+      media: this.media, // Gives access to video element from player
+      get muted() {
+        console.log(this);
+        return this.media.muted;
+      },
+      set muted(isMuted) {
+        this.media.muted = isMuted;
+      }
+    }
+
+    // console.log(player);
+    /**
+     {media: video.movie, play: ƒ, pause: ƒ}
+        media: video.movie
+        muted: (...) // Virtual property declared!!
+        pause: () => this.pause()
+        play: () => this.play()
+        get muted: ƒ muted()
+        set muted: ƒ muted(isMuted)
+        [[Prototype]]: Object
+     */
+
     this.plugins.forEach(plugin => {
-      plugin.run(this);
+      plugin.run(player);
     })
   }
 
